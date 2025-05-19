cc3bc60c3e7132b537da276ffa47545fd995d926
Isonade2 <c1004sos@naver.com>
2025-05-19 09:24:30 +0900

    init: 프로젝트 초기 설정
    - compose 파일 수정
    - security 비활성화

--- src/main/java/pokssak/gsg/GsgApplication.java
+++ src/main/java/pokssak/gsg/GsgApplication.java
@@ -2,9 +2,10 @@ package pokssak.gsg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GsgApplication {
 
     public static void main(String[] args) {

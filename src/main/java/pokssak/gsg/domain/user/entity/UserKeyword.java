519cea1f9c984ba173fa3b7d02d41e6966e1cc82
kimnoca <kimnoca@naver.com>
2025-05-19 22:37:31 +0900

    ci : ci 스크립트 작성
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정
    
    ci : ci 스크립트 수정

--- src/main/java/pokssak/gsg/domain/user/entity/UserKeyword.java
+++ src/main/java/pokssak/gsg/domain/user/entity/UserKeyword.java
@@ -1,7 +1,17 @@
 package pokssak.gsg.domain.user.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import pokssak.gsg.common.entity.BaseEntity;
 
 @Table(name = "user_keywords")

14ed67c738a47a63a95ff1328d77f27ff1bf7911
cupokki <nd1346@gmail.com>
2025-05-22 15:01:37 +0900

    fix: 컨트롤러 수정

--- src/main/java/pokssak/gsg/domain/review/controller/ReviewController.java
+++ src/main/java/pokssak/gsg/domain/review/controller/ReviewController.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import pokssak.gsg.common.dto.ApiResponse;
+import pokssak.gsg.domain.review.controller.api.ReviewControllerApi;
 import pokssak.gsg.domain.review.dto.ReviewCreateRequest;
+import pokssak.gsg.domain.review.dto.ReviewResponse;
 import pokssak.gsg.domain.review.service.ReviewService;
 import pokssak.gsg.domain.user.entity.User;
 import pokssak.gsg.domain.user.service.UserService;
@@ -17,43 +20,47 @@ import java.net.URI;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+
 @RestController
-public class ReviewController {
+public class ReviewController implements ReviewControllerApi {
 
     private final ReviewService reviewService;
     private final UserService userService;
 
     /** 모든 리뷰 조회 */
     @GetMapping("")
-    public ResponseEntity<Page<?>> getReviews(Pageable pageable){
-        var response = reviewService.getAllReviews(pageable);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ApiResponse<Page<ReviewResponse>>> getReviews(Pageable pageable){
+        var page = reviewService.getReviews(pageable);
+        return ResponseEntity.ok(ApiResponse.ok(page));
     }
 
     /** 리뷰 상세 조회 */
     @GetMapping("/{reviewId}")
-    public ResponseEntity<?> getReview(@PathVariable("reviewId") Long reviewId){
+    public ResponseEntity<ApiResponse<ReviewResponse>> getReview(@PathVariable("reviewId") Long reviewId){
         var response = reviewService.getReviewById(reviewId);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiResponse.ok(response));
     }
 
     /** 리뷰 추가 */
     @PostMapping("")
-    public ResponseEntity<?> getReview(
+    public ResponseEntity<ApiResponse<Void>> createReview(
             @AuthenticationPrincipal Long userId,
             @RequestBody ReviewCreateRequest reviewCreateRequest
     ){
+        userId = 1L; //TODO : 테스트용
         var reviewId = reviewService.createReview(userId, reviewCreateRequest);
         var uri = URI.create("/api/v1/reviews/" + reviewId);
-        return ResponseEntity.created(uri).build();
+        return ResponseEntity.created(uri)
+                .body(ApiResponse.ok(null));
     }
 
     /** 리뷰 삭제 */
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<?> deleteReviews(
+    public ResponseEntity<ApiResponse<Void>> deleteReview(
             @PathVariable("reviewId") Long reviewId
     ){
         reviewService.deleteReview(reviewId);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(ApiResponse.ok(null));
     }
 }

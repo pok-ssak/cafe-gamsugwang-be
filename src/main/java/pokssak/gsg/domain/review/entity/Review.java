d10364b5a5e22232e3cd57cd70607dbec1c44d9b
cupokki <nd1346@gmail.com>
2025-05-22 14:58:57 +0900

    feat: title 제거, cafeId 추가

--- src/main/java/pokssak/gsg/domain/review/entity/Review.java
+++ src/main/java/pokssak/gsg/domain/review/entity/Review.java
@@ -3,6 +3,7 @@ package pokssak.gsg.domain.review.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import pokssak.gsg.common.entity.BaseEntity;
+import pokssak.gsg.domain.cafe.entity.Cafe;
 import pokssak.gsg.domain.user.entity.User;
 
 @Table(name = "reviews")
@@ -20,13 +21,17 @@ public class Review extends BaseEntity {
     @Builder.Default
     private int rate = 0;
 
-    private String title;
-
     private String content;
 
     @Builder.Default
     private String imageUrl = "";
 
+    @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToOne
+    //, foreignKey = @ForeignKey(NO_CONSTRAINT)
+    @JoinColumn(name = "cafe_id", nullable = false)
+    private Cafe cafe;
 }

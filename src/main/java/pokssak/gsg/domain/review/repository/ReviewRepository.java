7e75d604bb1dd10384fd124fdd41c1adf641d4e5
cupokki <nd1346@gmail.com>
2025-05-22 14:59:45 +0900

    feat: 쿼리메서드 추가

--- src/main/java/pokssak/gsg/domain/review/repository/ReviewRepository.java
+++ src/main/java/pokssak/gsg/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,25 @@
 package pokssak.gsg.domain.review.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pokssak.gsg.domain.review.entity.Review;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByCafeId(Long cafeId, Pageable pageable);
+
+    Optional<Review> findTopByUserIdAndCafeIdOrderByModifiedAtDesc(Long userId, Long cafeId);
+
+//    @Query("""
+//            SELECT r
+//            FROM Review r
+//            WHERE r.user.id = :userId AND r.cafe.id = :cafeId
+//            ORDER BY r.modifiedAt DESC
+//            """)
+//    Optional<Review> findLatestByUserIdAndCafeId(@Param("userId") Long userId, @Param("cafeId")Long cafeId, Pageable pageable); //TODO : QueryDSL로
 }

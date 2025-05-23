38829a1fb67fa68bd33ca1431871d09e11214378
Isonade2 <67320022+Isonade2@users.noreply.github.com>
2025-05-23 09:43:14 +0900

    feat: 키워드, 위치 기반 카페 추천 기능 (#23)

--- src/main/java/pokssak/gsg/domain/cafe/controller/CafeController.java
+++ src/main/java/pokssak/gsg/domain/cafe/controller/CafeController.java
@@ -4,11 +4,9 @@ package pokssak.gsg.domain.cafe.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pokssak.gsg.common.dto.ApiResponse;
+import pokssak.gsg.domain.cafe.dto.RecommendResponse;
 import pokssak.gsg.domain.cafe.service.CafeService;
 
 import java.util.List;
@@ -28,5 +26,30 @@ public class CafeController {
         return ResponseEntity.ok(titles);
     }
 
+    @GetMapping("/recommend")
+    public ResponseEntity<?> recommend(
+            @RequestParam String option,
+            @RequestParam String keyword,
+            @RequestParam(required = false) Double lat,
+            @RequestParam(required = false) Double lon,
+            @RequestParam(defaultValue = "10") int limit){
 
+        log.info("keyword: {}, lat: {}, lon: {}, limit: {}", keyword, lat, lon, limit);
+        List<RecommendResponse> results;
+
+        switch (option) {
+            case "location":
+                results = cafeService.recommendByLocation(lat, lon, limit);
+                break;
+            case "keyword":
+                results = cafeService.recommendByKeyword(keyword, limit);
+                break;
+            case "hybrid":
+                results = cafeService.recommendByKeyword(keyword, limit);
+                break;
+            default:
+                return ResponseEntity.badRequest().body("invalid option");
+        }
+        return ResponseEntity.ok(ApiResponse.ok(results));
+    }
 }

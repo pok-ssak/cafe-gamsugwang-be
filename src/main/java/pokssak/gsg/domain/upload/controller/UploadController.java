ad2d11393003b2f6437981a1ab2b3f2809c39bb7
cupokki <nd1346@gmail.com>
2025-05-26 21:20:56 +0900

    feat: 업로드 API

--- src/main/java/pokssak/gsg/domain/upload/controller/UploadController.java
+++ src/main/java/pokssak/gsg/domain/upload/controller/UploadController.java
@@ -1,6 +1,5 @@
 package pokssak.gsg.domain.upload.controller;
 
-import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import pokssak.gsg.common.dto.ApiResponse;
+import pokssak.gsg.common.exception.CustomException;
 import pokssak.gsg.common.s3.S3Uploader;
-
+import pokssak.gsg.domain.upload.exception.UploadErrorCode;
 import java.net.URI;
 import java.util.Map;
 
@@ -24,11 +24,17 @@ public class UploadController {
     private final S3Uploader s3Uploader;
 
     @PostMapping("")
-    public ResponseEntity<?> upload(
-            @RequestPart(value = "file", required = true) MultipartFile file
+    public ResponseEntity<ApiResponse<?>> upload(
+            @RequestPart(value = "image", required = true) MultipartFile image
     ) {
-        log.info("fileName={}, fileType={}, fileSize={}", file.getOriginalFilename(), file.getContentType(), file.getSize());
-        var url = s3Uploader.upload(file);
+        log.info("fileName={}, fileType={}, fileSize={}",
+                image.getOriginalFilename(), image.getContentType(), image.getSize());
+        var type = image.getContentType();
+
+        if (!type.startsWith("image/"))
+            throw new CustomException(UploadErrorCode.NOT_AN_IMAGE);
+
+        var url = s3Uploader.upload(image);
         return ResponseEntity.created(URI.create(url))
                 .body(ApiResponse.ok(Map.of("url", url)));
     }

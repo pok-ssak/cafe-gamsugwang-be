048b0e2e6d4584269c2928454ae2ba8ce9473936
Isonade2 <c1004sos@naver.com>
2025-05-26 09:27:43 +0900

    feat: cafe 엔티티 생성

--- src/main/java/pokssak/gsg/domain/cafe/entity/Cafe.java
+++ src/main/java/pokssak/gsg/domain/cafe/entity/Cafe.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import pokssak.gsg.common.entity.BaseEntity;
 
+import java.math.BigDecimal;
+
 @Table(name = "cafes")
 @Getter
 @NoArgsConstructor
@@ -17,5 +19,27 @@ public class Cafe extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
+
+    private String info;
+
+    private String openTime;
+
+    private BigDecimal rate;
+
+    private Integer rateCount;
+
+    private String imgUrl;
+
+    private String address;
+
+    private String zipcode;
+
+    private BigDecimal x;
+    private BigDecimal y;
+
+    private String phone_number;
+
 
 }
--- src/main/java/pokssak/gsg/domain/cafe/repository/CafeRepository.java
+++ src/main/java/pokssak/gsg/domain/cafe/repository/CafeRepository.java
@@ -1,12 +1,10 @@
 package pokssak.gsg.domain.cafe.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pokssak.gsg.domain.cafe.entity.Cafe;
-import pokssak.gsg.domain.user.entity.User;
+
 
 @Repository
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
-
 }

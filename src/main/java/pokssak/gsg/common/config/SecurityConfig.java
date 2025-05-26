9fc56a98e737830325af477bbfe9a18728fb2bc3
Isonade2 <67320022+Isonade2@users.noreply.github.com>
2025-05-26 09:29:47 +0900

    feat: cafe 엔티티 생성 (#31)

--- src/main/java/pokssak/gsg/common/config/SecurityConfig.java
+++ src/main/java/pokssak/gsg/common/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         http.
             authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers("/api/v1/cafes/**").permitAll()
                 .anyRequest().authenticated()
             )
 

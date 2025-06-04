d47177ee1ab53510256eb4466823da73df58f914
kimnoca <kimnoca@naver.com>
2025-06-04 22:11:39 +0900

    ci : cd 스크립트 작성 및 Dockerfile 작성

--- src/main/java/pokssak/gsg/common/config/RedisConfig.java
+++ src/main/java/pokssak/gsg/common/config/RedisConfig.java
@@ -14,8 +14,8 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
-            RedisConnectionFactory redisConnectionFactory
-    ){
+        RedisConnectionFactory redisConnectionFactory
+    ) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());

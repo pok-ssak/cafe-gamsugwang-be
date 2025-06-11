c5b9323be538f7a728f0aae64d105adc90ed1d5f
cupokki <70307566+cupokki@users.noreply.github.com>
2025-06-11 10:42:43 +0900

    Update SecurityConfig.java
    config.setAllowedOrigins(List.of("http://localhost:3000"));

--- src/main/java/pokssak/gsg/common/config/SecurityConfig.java
+++ src/main/java/pokssak/gsg/common/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:3000"));
+                config.setAllowedOrigins(List.of("http://localhost:3000", "https://gamsugwang.store"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
